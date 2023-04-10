a4ba9836a36756640e60e25e2b66089665d99ce6
Paul-Nicolas Madelaine <pnm@pnm.tf>
2023-04-10 16:41:50 +0200

    initial commit

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    typhon.url = "github:typhon-ci/typhon";
+  };
+
+  outputs = { self, nixpkgs, typhon }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+      libTyphon = typhon.actions.${system};
+    in {
+      typhonProject = libTyphon.mkProject {
+        meta = {
+          title = "Test";
+          description = "testing Typhon";
+          homepage = "https://typhon-ci.org";
+        };
+        actions = {
+          jobsets = libTyphon.gitJobsets "https://github.com/typhon-ci/test";
+        };
+      };
+      typhonJobs = {
+        hello = pkgs.stdenv.mkDerivation {
+          name = "hello";
+          src = ./hello;
+          configurePhase = "export PREFIX=$out";
+        };
+        failing = pkgs.stdenv.mkDerivation {
+          name = "failing";
+        };
+      };
+    };
+}
